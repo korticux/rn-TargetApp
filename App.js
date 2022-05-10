cb51bab0cfb3bcfa990ae9dcd5fb4063d6a3d9c3
korticux <luis_cortina18@hotmail.com>
2022-05-09 21:23:21 -0700

    Colors.map error

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen/>;
+  return (
+    <LinearGradient style={styles.rootScreen}>
+      <StartGameScreen/>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    backgroundColor:'#ddb52f',
+    flex: 1,
+  }
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,10 +4,22 @@ import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
     return ( 
-    <View style={styles.inputContainerY}>
-        <TextInput/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.inputContainer}>
+        <TextInput 
+            style={styles.numberinput} 
+            maxLength={2}
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
+        <View style={styles.buttonsContainer}>
+           <View style={styles.buttonContainer}> 
+               <PrimaryButton>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+            <PrimaryButton>Confirm</PrimaryButton>
+            </View>
+        </View>
     </View>
     );
 }
@@ -16,10 +28,34 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
         padding: 16,
         marginTop: 100,
-        backgroundColor: '72063c',
-        marginHorizontal: 34,
-        borderRadius: 8
+        backgroundColor: '#4e0329',
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: .25
+    },
+    numberinput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        width: 50,
+        textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
