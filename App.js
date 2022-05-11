24fa1268c8ec2a6adfa8e38716e2c0f2a50dec4c
korticux <luis_cortina18@hotmail.com>
2022-05-10 23:33:38 -0700

    GameOverScreen

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber){
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gmaeOverHanlder() {
+    setGameIsOver(true);
   }
 
     let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
 
     if (userNumber) {
-      screen = <GameScreen/>;
+      screen = <GameScreen userNumber={userNumber} onGameOver= {gmaeOverHanlder}/>;
     }
 
+    if (gameIsOver && userNumber) {
+      screen = <GameOverScreen />
+    }
+
+  
+
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient colors={[ Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
       <ImageBackground
         source={require('./assets/images/adaptive-icon.png')} 
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+       <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
---
+++ components/ui/Title.js
@@ -0,0 +1,21 @@
+import { Text, StyleSheet} from 'react-native';
+
+import Colors from '../../constants/colors';
+
+function Title({children}){
+    return <Text style={styles.title}>Opponent's Guess</Text>
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: Colors.accent500,
+        textAlign: 'center',
+        borderColor: Colors.accent500,
+        borderWidth: 4,
+        padding: 12
+    }
+})
+
+export default Title;
